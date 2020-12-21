937018d7773930e933bf22f7d0a144d3b53ce770
Joey3001 <josephnero111@gmail.com>
2020-12-20 19:41:18 -0800

    Add index page for Vendors view

---
+++ Tracker.Solutions/Tracker/Controllers/VendorsController.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Tracker.Models;
+
+namespace Tracker.Controllers
+{
+  public class VendorsController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll(); 
+      return View(allVendors); 
+    }
+  }
+}
