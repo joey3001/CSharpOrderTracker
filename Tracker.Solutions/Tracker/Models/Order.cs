26fff237d4bea8b93c70b969dfb7d906c0e5204b
Joey3001 <josephnero111@gmail.com>
2020-12-20 16:07:53 -0800

    Add feature to Order.cs. Get good fail.

--- Tracker.Solutions/Tracker/Models/Order.cs
+++ Tracker.Solutions/Tracker/Models/Order.cs
@@ -4,11 +4,11 @@ namespace Tracker.Models
   public class Order
   {
     public string OrderTitle { get; set; }
-    //public string OrderDescription { get; set; }
-    public Order(string orderTitle) //string orderDescription)
+    public string OrderDescription { get; set; }
+    public Order(string orderTitle, string orderDescription)
     {
       OrderTitle = orderTitle;
-      //OrderDescription = orderDescription; 
+      OrderDescription = "Fail"; 
     }
   }
 }
