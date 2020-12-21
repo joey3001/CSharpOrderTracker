6b4dce2fab19cd2113fbdd9bffb9f546786fcd3a
Joey3001 <josephnero111@gmail.com>
2020-12-20 16:32:26 -0800

    Add & fail 6th test for Order.cs

--- Tracker.Solutions/Tracker.Tests/ModelTests/OrderTests.cs
+++ Tracker.Solutions/Tracker.Tests/ModelTests/OrderTests.cs
@@ -76,6 +76,26 @@ namespace Tracker.Tests
 
       Assert.AreEqual(orderDate, result); 
     }
+
+    [TestMethod]
+    public void GetAll_ReturnsListOfOrders_OrderList()
+    {
+      string orderTitle = "Test Title";
+      string orderDescription = "Test Description";
+      int orderPrice = 10;
+      string orderDate = "1/1/1900"; 
+      string orderTitle2 = "Test Title 2";
+      string orderDescription2 = "Test Description 2";
+      int orderPrice2 = 12;
+      string orderDate2 = "1/2/1900"; 
+      Order newOrder1 = new Order(orderTitle, orderDescription, orderPrice, orderDate); 
+      Order newOrder2 = new Order(orderTitle2, orderDescription2, orderPrice2, orderDate2);
+      List<Order> intendedList = new List<Order> { newOrder1, newOrder2 };
+
+      string result = Order.GetAll(); 
+
+      CollectionAssert.AreEqual(intendedList, result); 
+    }
   }
 }
 
