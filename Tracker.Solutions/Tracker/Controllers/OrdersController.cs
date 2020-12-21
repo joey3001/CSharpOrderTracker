ed4677d56bef60cb55c9abe0724cd5a255d7d702
Joey3001 <josephnero111@gmail.com>
2020-12-20 21:21:24 -0800

    Add feature to add orders to vendors

--- Tracker.Solutions/Tracker/Controllers/OrdersController.cs
+++ Tracker.Solutions/Tracker/Controllers/OrdersController.cs
@@ -1,8 +1,17 @@
 using System.Collections.Generic;
 using System;
 using Microsoft.AspNetCore.Mvc;
+using Tracker.Models;
 
 namespace Tracker.Controllers
 {
-  
+  public class OrdersController : Controller
+  {
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor); 
+    }
+  }
 }
