d3d0b244bf71cd5141500086eee8299aee646624
Joey3001 <josephnero111@gmail.com>
2020-12-20 20:02:09 -0800

    Add form to add vendors

--- Tracker.Solutions/Tracker/Controllers/VendorsController.cs
+++ Tracker.Solutions/Tracker/Controllers/VendorsController.cs
@@ -13,5 +13,18 @@ namespace Tracker.Controllers
       List<Vendor> allVendors = Vendor.GetAll(); 
       return View(allVendors); 
     }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return Redirect("/");
+    }
   }
 }
