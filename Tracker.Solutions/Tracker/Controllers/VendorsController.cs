a3249b8cd86c0b93bdf574ec58008885dc6adbf2
Joey3001 <josephnero111@gmail.com>
2020-12-20 20:59:26 -0800

    Added feature to click on vendor's and get more info

--- Tracker.Solutions/Tracker/Controllers/VendorsController.cs
+++ Tracker.Solutions/Tracker/Controllers/VendorsController.cs
@@ -26,5 +26,16 @@ namespace Tracker.Controllers
       Vendor newVendor = new Vendor(vendorName, vendorDescription);
       return Redirect("/");
     }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>(); 
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders; 
+      model.Add("vendor", selectedVendor); 
+      model.Add("orders", vendorOrders);
+      return View(model); 
+    }
   }
 }
